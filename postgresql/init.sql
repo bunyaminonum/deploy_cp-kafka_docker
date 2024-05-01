3d7d2e5e68250e0f504359b7984fab9b0bb77679
bunyaminonum <onumbunyamin47@gmail.com>
2024-05-01 19:53:32 +0300

    Revert "added init.db file"
    This reverts commit 6e92f257188b97adc4e4d0711a38ca99a33cd77b.

--- postgresql/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE table (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL
-);
-
-INSERT INTO table (name) VALUES ('Örnek Ad 1');
-INSERT INTO table (name) VALUES ('Örnek Ad 2');
-INSERT INTO table (name) VALUES ('Örnek Ad 3');
